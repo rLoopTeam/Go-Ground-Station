fe45954bd8dc7d3ea16b429172add95f02db2a96
nuttwerx <nofel.tiani@gmail.com>
2018-04-16 16:57:40 +0100

    script and readme work

--- vet_all.bat
+++ vet_all.bat
@@ -1,16 +1,34 @@
 @ECHO OFF
 ECHO This script will vet all packages in this project
 ECHO Vetting main package
-go vet rloop/Go-Ground-Station/
-IF ERRORLEVEL 0 (
-   ECHO No warnings found
-)
+go vet rloop/Go-Ground-Station/ 2> vet_results.txt
+CALL :CheckError
 ECHO Vetting constants package
-go vet rloop/Go-Ground-Station/constants
+go vet rloop/Go-Ground-Station/constants 2>> vet_results.txt
+CALL :CheckError
 ECHO Vetting datastore package
-go vet rloop/Go-Ground-Station/datastore
+go vet rloop/Go-Ground-Station/datastore 2>> vet_results.txt
+CALL :CheckError
 ECHO Vetting gsgrpc package
-go vet rloop/Go-Ground-Station/gsgrpc
+go vet rloop/Go-Ground-Station/gsgrpc 2>> vet_results.txt
+CALL :CheckError
 ECHO Vetting gstypes package
-go vet rloop/Go-Ground-Station/gstypes
+go vet rloop/Go-Ground-Station/gstypes 2>> vet_results.txt
+CALL :CheckError
+ECHO Vetting helpers package
+go vet rloop/Go-Ground-Station/helpers 2>> vet_results.txt
+CALL :CheckError
+ECHO Vetting logging package
+go vet rloop/Go-Ground-Station/logging 2>> vet_results.txt
+CALL :CheckError
+ECHO Vetting parsing package
+go vet rloop/Go-Ground-Station/parsing 2>> vet_results.txt
+CALL :CheckError
+ECHO Vetting server package
+go vet rloop/Go-Ground-Station/server 2>> vet_results.txt
+CALL :CheckError
 
+EXIT /B %ERRORLEVEL%
+
+:CheckError
+if %errorlevel% equ 0 ( echo No warnings found ) else ( echo warnings found and written to file )
